917eda7f03ba774f8582d97ef492fec66cc93c20
surilindur <surilindur@users.noreply.github.com>
2023-10-08 19:31:43 +0200

    Add workaround for context issue

--- experiments/ltqp-solid-join-restart/experiment.sh
+++ experiments/ltqp-solid-join-restart/experiment.sh
@@ -11,4 +11,15 @@ docker_file=./input/dockerfiles/catalogue.dockerfile
 docker build --network host --tag "$docker_tag" --file "$docker_file" --build-arg CONFIG_CATALOGUE=./input/config-catalogue.json .
 docker run --volume ./generated:/generated "$docker_tag"
 
+# this is a workaround for issue where jbr.js produces a directory
+rmdir combinations/combination_0/input/context-client.json/
+
+context='{
+  "sources": [],
+  "lenient": true
+}'
+
+echo "$context" > combinations/combination_0/input/context-client.json
+echo "$context" > combinations/combination_1/input/context-client.json
+
 yarn jbr run

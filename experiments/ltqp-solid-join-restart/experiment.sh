bc88b2eba34d420c409be7dbe5abb051f08d0bba
surilindur <surilindur@users.noreply.github.com>
2023-10-25 14:36:53 +0300

    Remove workarounds, run discover-8

--- experiments/ltqp-solid-join-restart/experiment.sh
+++ experiments/ltqp-solid-join-restart/experiment.sh
@@ -11,17 +11,6 @@ docker_file=./input/dockerfiles/catalogue.dockerfile
 docker build --network host --tag "$docker_tag" --file "$docker_file" .
 docker run --volume ./generated:/generated "$docker_tag"
 
-# this is a workaround for issue where jbr.js produces a directory
-# rmdir combinations/combination_0/input/context-client.json/
-
-# context='{
-#  "sources": [],
-#  "lenient": true
-# }'
-
-# echo "$context" > combinations/combination_0/input/context-client.json
-# echo "$context" > combinations/combination_1/input/context-client.json
-
 # this is to delete all queries that should not be run
 rm generated/out-queries/interactive-complex-*.sparql
 #rm generated/out-queries/interactive-short-*.sparql
@@ -30,6 +19,6 @@ rm generated/out-queries/interactive-complex-*.sparql
 #rm generated/out-queries/interactive-discover-4.sparql
 #rm generated/out-queries/interactive-discover-5.sparql
 #rm generated/out-queries/interactive-discover-7.sparql
-rm generated/out-queries/interactive-discover-8.sparql
+#rm generated/out-queries/interactive-discover-8.sparql
 
 yarn run jbr run

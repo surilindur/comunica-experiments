05bd418d0e89c05462f85268f6ed305d62e2a04a
surilindur <surilindur@users.noreply.github.com>
2024-03-04 16:42:19 +0000

    Fixes to experiment runner script

--- experiments/ltqp-solid-bloom-filters/experiment.sh
+++ experiments/ltqp-solid-bloom-filters/experiment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ ! -e ../../node_modules ]; then
+repo=$(realpath "$(pwd)/../..")
+
+if [ ! -e "$repo/node_modules" ]; then
     echo "Installing dependencies"
     yarn install --immutable
 fi
@@ -8,9 +10,17 @@ fi
 # Ensure the appropriate version of rdf-dataset-fragmenter is used by the SolidBench
 # of this specific experiment here. Otherwise it will use the NPM version.
 
-ln -sf ../../node_modules/rdf-dataset-fragmenter ../../node_modules/solidbench/node_modules/rdf-dataset-fragmenter
+source="$repo/node_modules/rdf-dataset-fragmenter"
+target="$repo/node_modules/solidbench/node_modules/rdf-dataset-fragmenter"
 
-if [ ! -e combinations ]; then
+echo "Linking:"
+echo "	from $source"
+echo "	to $target"
+
+rm -r "$target"
+ln -sf "$source" "$target"
+
+if [ ! -e generated/out-queries ]; then
     echo "Generating combinations"
     yarn run jbr generate-combinations
     # The validation step will fail, so it can be skipped by creating the directories

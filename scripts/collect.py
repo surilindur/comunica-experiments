1d849d8244db5ad0b867067b0d2c7b40c2cdb71e
surilindur <16085353+surilindur@users.noreply.github.com>
2025-05-08 22:57:10 +0200

    Update default experiment (#13)

--- scripts/collect.py
+++ scripts/collect.py
@@ -17,36 +17,38 @@ def collect_results(path: Path) -> None:
         rmtree(results_path)
     results_path.mkdir()
 
+    info(f"Collecting results into {results_path}")
+
     for combination in path.joinpath("combinations").iterdir():
         combination_output_path = combination.joinpath("output")
         combination_manifest_path = combination.joinpath("jbr-experiment.json")
 
         # Copy the original path, to avoid deleting stuff
-        results_path = results_path.joinpath(combination.name)
+        combination_results_path = results_path.joinpath(combination.name)
         copytree(
             combination_output_path,
-            results_path,
+            combination_results_path,
             dirs_exist_ok=False,
         )
         copy(
             combination_manifest_path,
-            results_path.joinpath(combination_manifest_path.name),
+            combination_results_path.joinpath(combination_manifest_path.name),
         )
         results_path.joinpath(".keep").unlink(missing_ok=True)
 
         # Compress the logs directory
-        logs_directory_path = results_path.joinpath("logs")
-        logs_archive_path = results_path.joinpath("logs.tar.gz")
+        logs_directory_path = combination_results_path.joinpath("logs")
+        logs_archive_path = combination_results_path.joinpath("logs.tar.xz")
         info(f"Compressing logs into {logs_archive_path}")
-        with open_tarfile(logs_archive_path, "w:gz") as tar:
+        with open_tarfile(logs_archive_path, "w:xz", preset=9) as tar:
             tar.add(name=logs_directory_path, arcname=".")
         rmtree(logs_directory_path)
 
         # Compress the stats
-        stats_archive_path = results_path.joinpath("stats.tar.gz")
+        stats_archive_path = combination_results_path.joinpath("stats.tar.xz")
         info(f"Compressing stats into {stats_archive_path}")
-        with open_tarfile(stats_archive_path, "w:gz") as tar:
-            for stats_file_path in results_path.iterdir():
+        with open_tarfile(stats_archive_path, "w:xz", preset=9) as tar:
+            for stats_file_path in combination_results_path.iterdir():
                 if stats_file_path.name.startswith("stats-"):
                     tar.add(name=stats_file_path, arcname=stats_file_path.name)
                     stats_file_path.unlink()

f72876dcb8f64257a4333d00879976152dcb6e65
surilindur <16085353+surilindur@users.noreply.github.com>
2025-07-18 23:50:03 +0300

    Update Wikidata experiment and results (#28)

--- processing/jbrcsv.py
+++ processing/jbrcsv.py
@@ -26,30 +26,56 @@ class JbrQuery:
         self.instance = row["id"]
         self.failed = True  # assumes failed, unless proven otherwise
         self.timestamps: NDArray[float64] = array([])
-        self.replication = int(row.get("replication", "1"))
+        self.replication = int(row.get("replication", "") or "1")
         if row.get("timestampsAll"):
             # parse the new query runner format file
             try:
-                self.timestamps = array([[t / 1000 for t in t_r] for t_r in loads(row["timestampsAll"])])
+                self.timestamps = array(
+                    [[t / 1000 for t in t_r] for t_r in loads(row["timestampsAll"])]
+                )
             except ValueError as ex:
                 assert "inhomogeneous shape after 1" in str(ex)
         elif row.get("timestamps"):
             # parse old query runner timestamps format
-            csv_timestamps = list(float(t) / 1000 for t in row["timestamps"].split(TIMESTAMP_DELIMITER))
+            csv_timestamps = list(
+                float(t) / 1000 for t in row["timestamps"].split(TIMESTAMP_DELIMITER)
+            )
             csv_timestamps.sort()
             self.timestamps = array([csv_timestamps])
         self.failed = len(self.timestamps.shape) < 2 or self.timestamps.shape[1] < 1
-        self.duration_avg = float(row["time"]) / 1000
-        self.duration_min = float(row["timeMin"]) / 1000 if "timeMin" in row else self.duration_avg
-        self.duration_max = float(row["timeMax"]) / 1000 if "timeMax" in row else self.duration_avg
-        self.results_avg = float(row["results"])
-        self.results_min = float(row["resultsMin"]) if "resultsMin" in row else self.results_avg
-        self.results_max = float(row["resultsMax"]) if "resultsMax" in row else self.results_avg
+        self.duration_avg = float(row.get("time") or "0") / 1000
+        row_time_min = row.get("timeMin")
+        self.duration_min = (
+            float(row_time_min) / 1000 if row_time_min else self.duration_avg
+        )
+        row_time_max = row.get("timeMax")
+        self.duration_max = (
+            float(row_time_max) / 1000 if row_time_max else self.duration_avg
+        )
+        self.results_avg = float(row.get("results") or "0")
+        row_results_min = row.get("resultsMin")
+        self.results_min = (
+            float(row_results_min) if row_results_min else self.results_avg
+        )
+        row_results_max = row.get("resultsMax")
+        self.results_max = (
+            float(row_results_max) if row_results_max else self.results_avg
+        )
         self.http_requests_avg = float(row["httpRequests"] or "0")
-        self.http_requests_min = float(row["httpRequestsMin"] or "0") if "httpRequestsMin" in row else self.http_requests_avg
-        self.http_requests_max = float(row["httpRequestsMax"] or "0") if "httpRequestsMax" in row else self.http_requests_avg
+        self.http_requests_min = (
+            float(row["httpRequestsMin"] or "0")
+            if "httpRequestsMin" in row
+            else self.http_requests_avg
+        )
+        self.http_requests_max = (
+            float(row["httpRequestsMax"] or "0")
+            if "httpRequestsMax" in row
+            else self.http_requests_avg
+        )
         if not self.failed and row["error"] == "true":
-            warning(f"Accepting failed query {self.name} with error {row.get("errorDescription")}")
+            warning(
+                f"Accepting failed query {self.name} with error {row.get("errorDescription")}"
+            )
 
     def __hash__(self) -> int:
         return hash(self.name)
@@ -64,6 +90,7 @@ class JbrQuery:
     @property
     def name(self) -> str:
         return f"{self.template}-{self.instance}"
+
 
 def parse_jbr_csv(path: Path) -> Set[JbrQuery]:
     """Parses the given query-times.csv into a set of result abstractions."""
